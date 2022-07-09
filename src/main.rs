4a6ff92adab1c774b4ee5c97a93baae4abd880fb
Shirin Anlen <sanlen@SHIRIN-MBP.lan>
2022-07-09 19:20:13 -0400

    add some comments

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,60 @@ use c2pa::{
 use std::path::PathBuf;
 use std::fs::File;
 
+// output file
 const DESTINATION_PATH: &str = "/tmp/claims/test-c2pa-trump-trial-with-claims.jpeg";
+
+// source image
 const SOURCE_PATH: &str = "./jan6.jpeg";
 
 fn write_assertion() -> Result<(), Error> {
+    // init sample manifest
     let mut manifest = Manifest::new("test_app".to_owned());
  
+    /**
+     * add a simple action to the manifest
+     * this isn't a great action since it's probably a value already
+     * included in the library but it's a test val that we can write
+     * and read back later on
+     */
     let actions = Actions::new().add_action(
         Action::new(c2pa_action::CREATED)
         .set_parameter("at", "07102022".to_owned())?
     );
 
+    // add the assertion to the manifest
     let result = manifest.add_assertion(&actions);
 
     if !result.is_ok() {
         println!("invalid assertion: {:?}", result.err());
     }
+
+    /**
+     * now that the manifest is made we need to generate a new image with the signed manifest
+     */
+
+
     let source = PathBuf::from(SOURCE_PATH);
     let path = PathBuf::from(DESTINATION_PATH);
 
 
+    /**
+     * pull in the certificate and private key
+     * I had difficulty generating these (since creating a self-signed cert is not straightforward), so I
+     * pulled the pub/pem files from the repo examples
+     */
     let signcert_path  = "/tmp/keys/c2pa.pub";
     let pkey_path = "/tmp/keys/c2pa.pem";
+
+    // note that to use the `get_signer_from_files, the `file_io` feature needs to be enabled
     let signer_result = get_signer_from_files(signcert_path, pkey_path, "ps256", None);
 
 
+    // this is some extra logging to debug
     match signer_result {
         Err(err) => println!("got an issue {:?}", err),
         Ok(result) => {
+            // embed the manifest in the destination image file via the signer created above
             let other_result = manifest.embed(&source, &path, &*result);
             if !other_result.is_ok() {
                 println!("whoopsie {:?}", other_result.err());
@@ -49,12 +75,21 @@ fn write_assertion() -> Result<(), Error> {
 }
 
 fn read_source() -> Result<(), Error> {
+    // retrieve the manifest embedded in the destination image asset
     let store = ManifestStore::from_file(SOURCE_PATH)?;
+
+    // print out the entire store since it is nicely formatted here
     println!("{}", store);
 
     if let Some(manifest) = store.get_active() {
+        /**
+         * fetch a bucket of actions (and organize by label?)
+         * this bit was unclear to me.  not entirely sure what "label"
+         * is here, but it allows us to get out what we put in above.
+         */
         let actions: Actions = manifest.find_assertion(Actions::LABEL)?;
         for action in actions.actions {
+            // this should print the "created" we wrote to the manifest above
             println!("got an action: {}\n", action.action());
         }
     }
