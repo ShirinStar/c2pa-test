086fc8e7e127e306586830dd6bfd0ea318888732
Shirin Anlen <sanlen@SHIRIN-MBP.lan>
2022-07-09 19:22:33 -0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use c2pa::{
-    assertions::{User, Action, Actions, c2pa_action},
-    Manifest, Error,
-    get_signer_from_files, 
-    ManifestStore
+    assertions::{c2pa_action, Action, Actions, User},
+    get_signer_from_files, Error, Manifest, ManifestStore,
 };
 
-use std::path::PathBuf;
 use std::fs::File;
+use std::path::PathBuf;
 
 // output file
 const DESTINATION_PATH: &str = "/tmp/claims/test-c2pa-trump-trial-with-claims.jpeg";
@@ -17,17 +15,15 @@ const SOURCE_PATH: &str = "./jan6.jpeg";
 fn write_assertion() -> Result<(), Error> {
     // init sample manifest
     let mut manifest = Manifest::new("test_app".to_owned());
- 
+
     /**
      * add a simple action to the manifest
      * this isn't a great action since it's probably a value already
      * included in the library but it's a test val that we can write
      * and read back later on
      */
-    let actions = Actions::new().add_action(
-        Action::new(c2pa_action::CREATED)
-        .set_parameter("at", "07102022".to_owned())?
-    );
+    let actions = Actions::new()
+        .add_action(Action::new(c2pa_action::CREATED).set_parameter("at", "07102022".to_owned())?);
 
     // add the assertion to the manifest
     let result = manifest.add_assertion(&actions);
@@ -39,23 +35,19 @@ fn write_assertion() -> Result<(), Error> {
     /**
      * now that the manifest is made we need to generate a new image with the signed manifest
      */
-
-
     let source = PathBuf::from(SOURCE_PATH);
     let path = PathBuf::from(DESTINATION_PATH);
-
 
     /**
      * pull in the certificate and private key
      * I had difficulty generating these (since creating a self-signed cert is not straightforward), so I
      * pulled the pub/pem files from the repo examples
      */
-    let signcert_path  = "/tmp/keys/c2pa.pub";
+    let signcert_path = "/tmp/keys/c2pa.pub";
     let pkey_path = "/tmp/keys/c2pa.pem";
 
     // note that to use the `get_signer_from_files, the `file_io` feature needs to be enabled
     let signer_result = get_signer_from_files(signcert_path, pkey_path, "ps256", None);
-
 
     // this is some extra logging to debug
     match signer_result {
@@ -69,27 +61,22 @@ fn write_assertion() -> Result<(), Error> {
         }
     }
 
-
-
     Ok(())
 }
 
 fn read_source() -> Result<(), Error> {
-    // retrieve the manifest embedded in the destination image asset
+    /**
+     * this will not return anything
+     * but it shows the original image file did not have the generated claims
+     * and is not altered during this process
+     */
     let store = ManifestStore::from_file(SOURCE_PATH)?;
 
-    // print out the entire store since it is nicely formatted here
     println!("{}", store);
 
     if let Some(manifest) = store.get_active() {
-        /**
-         * fetch a bucket of actions (and organize by label?)
-         * this bit was unclear to me.  not entirely sure what "label"
-         * is here, but it allows us to get out what we put in above.
-         */
         let actions: Actions = manifest.find_assertion(Actions::LABEL)?;
         for action in actions.actions {
-            // this should print the "created" we wrote to the manifest above
             println!("got an action: {}\n", action.action());
         }
     }
@@ -97,24 +84,38 @@ fn read_source() -> Result<(), Error> {
 }
 
 fn read_dest() -> Result<(), Error> {
-        let store = ManifestStore::from_file(DESTINATION_PATH)?;
-        println!("printing store: {}", store);
+    // retrieve the manifest embedded in the destination image asset
 
-        if let Some(manifest) = store.get_active() {
-            let actions_result: Result<Actions, Error> = manifest.find_assertion(Actions::LABEL);
-            match actions_result {
-                Ok(actions) => {
-                    for action in actions.actions {
-                        println!("got an action: {}\n", action.action());
-                        println!("action stuff: {:?}", action);
-                    }
-                },
-                Err(err) => {
-                    println!("had an issue getting the assertion with actions label: {:?}", err);
+    let store = ManifestStore::from_file(DESTINATION_PATH)?;
+
+    // print out the entire store since it is nicely formatted here
+
+    println!("printing store: {}", store);
+
+    if let Some(manifest) = store.get_active() {
+        /**
+         * fetch a bucket of actions (and organize by label?)
+         * this bit was unclear to me.  not entirely sure what "label"
+         * is here, but it allows us to get out what we put in above.
+         */
+        let actions_result: Result<Actions, Error> = manifest.find_assertion(Actions::LABEL);
+        match actions_result {
+            Ok(actions) => {
+                // this should print the "created" we wrote to the manifest above
+
+                for action in actions.actions {
+                    println!("got an action: {}\n", action.action());
+                    println!("action stuff: {:?}", action);
                 }
             }
-
-        } 
+            Err(err) => {
+                println!(
+                    "had an issue getting the assertion with actions label: {:?}",
+                    err
+                );
+            }
+        }
+    }
     Ok(())
 }
 
@@ -122,14 +123,20 @@ fn main() -> Result<(), Error> {
     println!("writing assertion to new file");
     let write_result = write_assertion();
     if write_result.is_err() {
-        println!("encountered an error writing the assertion: {:?}", write_result.err());
+        println!(
+            "encountered an error writing the assertion: {:?}",
+            write_result.err()
+        );
     }
 
     println!("reading original source for manifest / claims");
     let result = read_source();
 
     if result.is_err() {
-        println!("had an issue reading a manifest from the first source: {:?}", result.err());
+        println!(
+            "had an issue reading a manifest from the first source: {:?}",
+            result.err()
+        );
     }
 
     println!("reading new generated file with manifest");
