fb0a9c2a748719eed6cbc3a3996c2e82e62412b2
Shirin Anlen <sanlen@SHIRIN-MBP.lan>
2022-07-09 19:05:48 -0400

    sample c2pa

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,108 @@
-fn main() {
-    println!("Hello, world!");
-    println!("hey");
+use c2pa::{
+    assertions::{User, Action, Actions, c2pa_action},
+    Manifest, Error,
+    get_signer_from_files, 
+    ManifestStore
+};
+
+use std::path::PathBuf;
+use std::fs::File;
+
+const DESTINATION_PATH: &str = "/tmp/claims/test-c2pa-trump-trial-with-claims.jpeg";
+const SOURCE_PATH: &str = "./jan6.jpeg";
+
+fn write_assertion() -> Result<(), Error> {
+    let mut manifest = Manifest::new("test_app".to_owned());
+ 
+    let actions = Actions::new().add_action(
+        Action::new(c2pa_action::CREATED)
+        .set_parameter("at", "07102022".to_owned())?
+    );
+
+    let result = manifest.add_assertion(&actions);
+
+    if !result.is_ok() {
+        println!("invalid assertion: {:?}", result.err());
+    }
+    let source = PathBuf::from(SOURCE_PATH);
+    let path = PathBuf::from(DESTINATION_PATH);
+
+
+    let signcert_path  = "/tmp/keys/c2pa.pub";
+    let pkey_path = "/tmp/keys/c2pa.pem";
+    let signer_result = get_signer_from_files(signcert_path, pkey_path, "ps256", None);
+
+
+    match signer_result {
+        Err(err) => println!("got an issue {:?}", err),
+        Ok(result) => {
+            let other_result = manifest.embed(&source, &path, &*result);
+            if !other_result.is_ok() {
+                println!("whoopsie {:?}", other_result.err());
+            }
+        }
+    }
+
+
+
+    Ok(())
+}
+
+fn read_source() -> Result<(), Error> {
+    let store = ManifestStore::from_file(SOURCE_PATH)?;
+    println!("{}", store);
+
+    if let Some(manifest) = store.get_active() {
+        let actions: Actions = manifest.find_assertion(Actions::LABEL)?;
+        for action in actions.actions {
+            println!("got an action: {}\n", action.action());
+        }
+    }
+    Ok(())
+}
+
+fn read_dest() -> Result<(), Error> {
+        let store = ManifestStore::from_file(DESTINATION_PATH)?;
+        println!("printing store: {}", store);
+
+        if let Some(manifest) = store.get_active() {
+            let actions_result: Result<Actions, Error> = manifest.find_assertion(Actions::LABEL);
+            match actions_result {
+                Ok(actions) => {
+                    for action in actions.actions {
+                        println!("got an action: {}\n", action.action());
+                        println!("action stuff: {:?}", action);
+                    }
+                },
+                Err(err) => {
+                    println!("had an issue getting the assertion with actions label: {:?}", err);
+                }
+            }
+
+        } 
+    Ok(())
+}
+
+fn main() -> Result<(), Error> {
+    println!("writing assertion to new file");
+    let write_result = write_assertion();
+    if write_result.is_err() {
+        println!("encountered an error writing the assertion: {:?}", write_result.err());
+    }
+
+    println!("reading original source for manifest / claims");
+    let result = read_source();
+
+    if result.is_err() {
+        println!("had an issue reading a manifest from the first source: {:?}", result.err());
+    }
+
+    println!("reading new generated file with manifest");
+    let result = read_dest();
+    if result.is_err() {
+        println!("had an issue reading generated file: {:?}", result.err());
+    } else {
+        println!("destination ok");
+    }
+    Ok(())
 }
